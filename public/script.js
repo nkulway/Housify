70f8659df272ac15abbabc1b12516b79eb635c75
Nick Kulway <nkulway@gmail.com>
2021-11-09 19:48:54 -0500

    commit before merge

--- public/script.js
+++ public/script.js
@@ -1,83 +1,8 @@
-
-// const handleSubmit = (e) => {
-//     e.preventDefault();
-//     const data = new FormData(e.target);
-//     const body = formatFormData(data);
-//     postData("http://localhost:3000/users/add", body).then((response) =>
-//       console.log(response)
-//     );
-//   };
-
-//   function formatFormData(fd) {
-//     const data = {};
-//     for (let key of fd.keys()) {
-//       data[key] = fd.get(key);
-//     }
-//     return data;;
-//   }
-
-// //   const form = document.getElementById("form");
-// // form.addEventListener("submit", handleSubmit);
-
-//   async function postData(url, body) {
-//     const response = await fetch(url, {
-//       method: "POST", // *GET, POST, PUT, DELETE, etc.
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(body, null, 4)
-//     });
-//     return response.json();
-//   }
-
-//   const contactList = document.getElementById("contactList");
-//   contactList.addEventListener("click", handleClick);
-
-  // function handleClick(e) {
-  //   const target = e.target;
-  //   const id = target.id;
-
-  //   if (target.tagName === 'BUTTON'){
-  //     const parent = target.parentElement
-  //     const body = {
-  //       id: target.getAttribute('data-id'),
-  //       name: parent.querySelector('input').value
-  //     }
-  //     deleteRow(`http://localhost:3000/users/edit`, body)
-  //         .then(res => console.log(res.message))
-  //         .catch(err => console.log(err));
-  //   } else {
-  //     target.innerHTML = `
-  //     <input value="${target.innerText}" />
-  //     <button data-id="${id}">Update</button>
-  //   `;
-  //   }}
-
-// function handleClick(e) {
-//   const target = e.target;
-//   const id = target.id;
-
-//   if (target.tagName === "BUTTON") {
-//     const parent = target.parentElement;
-//     const body = {
-//       id: target.getAttribute("data-id"),
-//         };
-//     deleteData(`http://localhost:3000/users/leave`, body)
-//       .then((res) => console.log(res.message))
-//       .catch((err) => console.log(err));
-//   } else {
-//     target.innerHTML = `
-//     <input value="${target.innerText}" />
-//     <button data-id="${id}">Delete</button> 
-//   `;
-//   }
-// }
-
 const handleSubmit = (e) => {
   e.preventDefault();
   const data = new FormData(e.target);
   const body = formatFormData(data);
-  postData("http://localhost:3000/users/add", body).then((response) =>
+  postData("http://localhost:3000/chores/add-chore", body).then((response) =>
     console.log(response)
   );
 };
@@ -107,36 +32,9 @@ async function postData(url, body) {
   return response.json();
 }
 
-// const contactList = document.getElementById("contactList");
-// contactList.addEventListener("click", handleClick);
-// const deleteUser = () => {
-//   axios.deleteData(`http://localhost:3000/users/:id`)
-// }
-
-// function handleClick(e) {
-//     const target = e.target;
-//     const id = target.id;
   
-//     if (target.tagName === "BUTTON") {
-//       const body = {
-//         id
-//           };
-//       deleteData(`http://localhost:3000/users/:id`, body)
-//         .then((res) => console.log(res.message))
-//         .catch((err) => console.log(err));
-//     }
-    
-//     }
-  
-
 const removeUser = document.querySelector(".delete");
 removeUser.addEventListener("click", deleteData);
-
-
-function test(){
-alert('it works')
-}
-
 
 
 async function deleteData(url, body) {
@@ -151,17 +49,3 @@ async function deleteData(url, body) {
   });
   return response.json();
 }
-
-
-// async function deleteData() {
-//   const response = await fetch('http://localhost:3000/users/:id', {
-//     method: "DELETE", // *GET, POST, PUT, DELETE, etc.
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     // body: JSON.stringify(body, null, 4),
-//   });
-//   return response.json();
-//}
-
-
--- routes/chores.js
+++ routes/chores.js
@@ -1,10 +1,57 @@
 var express = require('express');
 var router = express.Router();
 const Sequelize = require('sequelize')
-const { Member } = require('../models')
+const { Chore } = require('../models')
+const chore = require('../models/chore');
 
 router.get('/', (req, res) => {
     res.send('respond with a resource')
 })
+
+// router.get('/list', (req, res) => {
+//     res.render('choreList')
+// })
+
+router.get('/add-chore', (req, res) => {
+    res.render('addChore')
+})
+
+router.post("/add-chore", async (req, res) => {
+    const { chore, day } = req.body;
+  
+    const newUser = await Chore.create({
+      chore,
+      day
+    });
+  
+    res.json({
+      id: newUser.id,
+    });
+  });
+
+  router.get("/list", async (req, res) => {
+    // requesting all of the data from our 'Members' database
+  const todo = await Chore.findAll();
+  const html = todo
+    .map((chores) => {
+            // isolate the id inside of the list to make them unique "member.id"
+            // diplay names of people from db
+      return `
+      <li data-id="${chores.id}">
+      <button id="${chores.id}" class="delete">${chores.id}</button>
+      ${chores.id}
+      </li>
+      `;
+    })
+    .join("");
+  console.log('choreList')
+  res.render("choreList", {
+    locals: {
+      todo: html,
+    },
+  });
+  });
+
+
 
 module.exports = router;
