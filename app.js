dbdb9f83451243bf87d3ad53c576c23e0f9d2273
Lakeysha <82991137+lakeyshaa@users.noreply.github.com>
2021-11-09 19:22:06 -0500

    add partials

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'html');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const choresRouter = require('./routes/choresroute');
+const cRouter = require('./routes/choresRoute');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/chores', choresRouter);
+app.use('/chores', cRouter);
 
 
 
